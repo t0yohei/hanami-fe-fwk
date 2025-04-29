a2c51f0ad5adff112998b8a0d0c49a27a91825c9
t0yohei <inuneko690121@gmail.com>
2025-04-29 12:14:14 +0900

    add ch3 h function

---
+++ packages/runtime/src/h.js
@@ -0,0 +1,36 @@
+export const DOM_TYPES = {
+  TEXT: 'text',
+  ELEMENT: 'element',
+  FRAGMENT: 'fragment',
+}
+
+export function h(tag, props = {}, children = []) {
+  return {
+    tag,
+    props,
+    children: mapTextNodes(withoutNulls(children)),
+    type: DOM_TYPES.ELEMENT,
+  }
+}
+
+export function mapTextNodes(children) {
+  return children.map((child) => (typeof child === "string" ? hString(child) : child));
+}
+
+export function withoutNulls(arr) {
+  return arr.filter((item) => item !== null);
+}
+
+export function hString(str) {
+  return {
+    type: DOM_TYPES.TEXT,
+    value: str,
+  }
+}
+
+export function hFragment(vNodes) {
+  return {
+    type: DOM_TYPES.FRAGMENT,
+    children: mapTextNodes(withoutNulls(vNodes)),
+  };
+}
