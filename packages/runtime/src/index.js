329e01b8309a90e63089cd8016599d54aa69a59d
t0yohei <inuneko690121@gmail.com>
2025-06-09 18:20:28 +0900

    add ch14 nextTick

--- packages/runtime/src/index.js
+++ packages/runtime/src/index.js
@@ -1,3 +1,4 @@
 export { createApp } from "./app";
 export { defineComponent } from "./component.js";
 export { DOM_TYPES, h, hFragment, hString } from "./h";
+export { nextTick } from "./scheduler.js";
